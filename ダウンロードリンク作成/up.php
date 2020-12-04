21234ae67a84cfc59369d7f9b8758c092f369cd1
rinkunnn-hp <75443958+rinkunnn-hp@users.noreply.github.com>
2020-12-04 16:23:33 +0900

    Update up.php

--- ダウンロードリンク作成/up.php
+++ ダウンロードリンク作成/up.php
@@ -9,7 +9,7 @@
 
 if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
   if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "files/" . $_FILES["upfile"]["name"])) {
-    chmod("files/" . $_FILES["upfile"]["name"], 0644);
+    chmod("files/" . $_FILES["upfile"]["name"], 0777);
     echo $_FILES["upfile"]["name"] . "をアップロードしました。";
   } else {
     echo "ファイルをアップロードできません。";
