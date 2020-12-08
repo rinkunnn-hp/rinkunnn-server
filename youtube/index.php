0ca4e31290da62f05b7bf5de64cca71d622cd3d4
rinkunnn-hp <75443958+rinkunnn-hp@users.noreply.github.com>
2020-12-09 01:02:40 +0900

    Update index.php

--- youtube/index.php
+++ youtube/index.php
@@ -89,7 +89,7 @@
                 readfile("$file_path1");
         }
 ?>
-<iframe src="https://www.youtube.com" width=600vw></iframe>
+<iframe src="www.youtube.com" width=600vw></iframe>
 </div>
 </body>
 </html>
